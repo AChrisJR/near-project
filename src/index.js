f89853ff633951a2d5daaeada122a6cb788e8d94
Tomas Rodríguez <trodrigueza@unal.edu.co>
2021-11-23 20:56:27 -0500

    version compartida

--- src/index.js
+++ src/index.js
@@ -70,10 +70,6 @@ function signedOutFlow() {
   document.querySelector('#signed-out-flow').style.display = 'block'
 }
 
-function kollaFlow() {
-  document.querySelector('#kolla-flow').style.display = 'block'
-}
-
 // Displaying the signed in flow container and fill in account-specific data
 function signedInFlow() {
   document.querySelector('#signed-in-flow').style.display = 'block'
@@ -116,3 +112,47 @@ window.nearInitPromise = initContract()
     else signedOutFlow()
   })
   .catch(console.error)
+
+  //
+
+  const { connect, transactions, keyStores } = require("near-api-js");
+  const fs = require("fs");
+  const path = require("path");
+  const homedir = require("os").homedir();
+  
+  const CREDENTIALS_DIR = ".near-credentials";
+  // NOTE: replace "example" with your accountId
+  const CONTRACT_NAME = `contract.${window.accountId}`;
+  const WHITELIST_ACCOUNT_ID = "whitelisted-account.example.testnet";
+  const WASM_PATH = path.join(__dirname, "../utils/wasm-files/staking_pool_factory.wasm");
+  
+  const credentialsPath = path.join(homedir, CREDENTIALS_DIR);
+  const keyStore = new keyStores.UnencryptedFileSystemKeyStore(credentialsPath);
+  
+  const config = {
+      keyStore,
+      networkId: "testnet",
+      nodeUrl: "https://rpc.testnet.near.org",
+  };
+  
+  sendTransactions();
+  
+  async function sendTransactions() {
+      const near = await connect({ ...config, keyStore });
+      const account = await near.account(CONTRACT_NAME);
+      const newArgs = { staking_pool_whitelist_account_id: WHITELIST_ACCOUNT_ID };
+      const result = await account.signAndSendTransaction({
+          receiverId: CONTRACT_NAME,
+          actions: [
+              transactions.deployContract(fs.readFileSync(WASM_PATH)),
+              transactions.functionCall(
+                  "new",
+                  Buffer.from(JSON.stringify(newArgs)),
+                  10000000000000,
+                  "0"
+              ),
+          ],
+      });
+  
+      console.log(result);
+  }
